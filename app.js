79f8a0c683ed1a77a75c078ce3ca28b8875dc21e
岩本将史 <iwamotomasashi@iwamotomasashinoMacBook-Pro.local>
2022-03-18 21:26:35 +0900

    コメント

--- app.js
+++ app.js
@@ -5,9 +5,64 @@ const app = express();
 //ルーティング
 //「http://localhost:3000/」を指定したときに、「index.ejs」を表示する
 app.get('/', (req, res) => {
-  res.render('a.ejs');
+    //PostreSQL接続
+    connection.query(
+        'SELECT * FROM testtb', //発行するクエリ
+        (error,result) => {
+          if(error) throw error;
+          res.render('a.ejs',{items:result.rows}); //クエリ結果をitemsとしてindex.ejsに渡す
+        }
+      );
 });
 
+// -----------------------------------------------
+//      postgresql設定
+// -----------------------------------------------
+const { Client } = require('pg');
+
+// --------  接続情報  -----------
+const connection = new Client({
+  //接続先DB情報を記述
+  host: 'localhost',
+  user: 'iwamotomasashi',
+  password: 'masashi1226',
+  database: 'postgres'
+});
+
+// --------  接続  -----------
+connection.connect((err) => {
+  //接続できなかったとき、エラーをコンソールに表示させる
+  if(err){
+      console.log('error connecting:' + err.stack);
+      return;
+  }
+  //今回はわかりやすいように、接続に成功したらコンソールに「success」と表示されるようにします
+  console.log('success');
+});
+
+// -----------------------------------------------
+//      postgresql設定
+// -----------------------------------------------
+const { Client } = require('pg');
+require('dotenv').config({debug:true});  //ここに追加
+
+const connection = new Client({
+  connectionString: process.env.DATABASE_URL  //DB情報を環境変数DATABASE_URLに変更
+});
+// -----------------------------------------------
+//      postgresql設定
+// -----------------------------------------------
+const { Client } = require('pg');
+require('dotenv').config({debug:true});
+
+const connection = new Client({
+  connectionString: process.env.DATABASE_URL,
+  //SSLを設定
+  ssl: {
+    sslmode:'require',
+    rejectUnauthorized:false
+  }
+});
 // -----------------------------------------------
 //      port
 // -----------------------------------------------
