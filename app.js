0fcc1f75a1738c31b0ecd23b1e3029e5a2d1db07
岩本将史 <iwamotomasashi@iwamotomasashinoMacBook-Pro.local>
2022-03-20 19:23:40 +0900

    コメント

--- app.js
+++ app.js
@@ -2,17 +2,85 @@
 const express = require('express');
 const app = express();
 
-//ルーティング
-//「http://localhost:3000/」を指定したときに、「index.ejs」を表示する
-app.get('/', (req, res) => {
-    //PostreSQL接続
+
+app.use(express.static('public'));
+
+
+//ホーム画面を指定したときに、「index.ejs」を表示する
+app.get('/',(req,res)=>{
+    const sql = "select * from datetime";
+    connection.query(sql, (err, result, fields) =>{ 
+        if(err)throw err;
+        //console.log(result);
+    })
+    res.render('top.ejs');
+});
+
+app.get('/index',(req,res)=>{
+    res.render('index.ejs');
+    //console.log(now.toFormat('M/D/YY'));
+});
+
+app.get('/start',(req,res)=>{
+    var now = new Date();
     connection.query(
-        'SELECT * FROM datetime', //発行するクエリ
-        (error,result) => {
-          if(error) throw error;
-          res.render('a.ejs',{items:result.rows}); //クエリ結果をitemsとしてindex.ejsに渡す
-        }
-      );
+        "insert into datetime (date, startime,finishtime,resttime,worktime) values(?,?,?,?,?)",
+        [now.toFormat("YYYY-MM-DD"),now.toFormat('HH24:MI:SS'),"000000","000000","000000"],
+        (err, result, fields) =>{ 
+            if(err)throw err
+            res.redirect('/exit');
+        });
+    });
+
+app.get('/exit',(req,res)=>{
+    res.render('exit.ejs');
+    //console.log(now.toFormat('M/D/YY'));
+});
+
+app.get('/rest',(req,res)=>{
+    res.render('rest.ejs');
+    //console.log(now.toFormat('M/D/YY'));
+});
+
+app.get('/finish',(req,res)=>{
+    
+    var now = new Date();
+    connection.query(
+        "update datetime set finishtime = ? ORDER BY id DESC LIMIT 1",
+        [now.toFormat('HH24:MI:SS')],
+        (err, result, fields) =>{ 
+            if(err)throw err
+            res.redirect('/finish2');
+        });
+    });
+
+app.get('/finish2',(req,res)=>{
+    connection.query(
+        "update datetime set worktime = timediff(finishtime, startime) ORDER BY id DESC LIMIT 1",
+        (err, result, fields) =>{ 
+            if(err)throw err
+            res.redirect('/conclude');
+        });
+    });
+
+app.get('/conclude',(req,res)=>{
+    const sql = "select * from datetime ORDER BY id DESC LIMIT 1";
+    connection.query(sql, (err, result, fields) =>{ 
+        if(err)throw err
+        //console.log(result)
+        res.render('conclude.ejs', {datas: result})
+    });
+    //console.log(now.toFormat('M/D/YY'));
+});
+
+app.get('/timecard',(req,res)=>{
+    const sql = "select * from datetime order by id desc";
+    connection.query(sql, (err, result, fields) =>{ 
+        if(err)throw err
+        //console.log(result)
+        res.render('timecard.ejs', {cards: result})
+    });
+    //console.log(now.toFormat('M/D/YY'));
 });
 
 // -----------------------------------------------
